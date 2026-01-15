7bc2e6b221c2faef4377fa73a5fd5a262eb0c492
Connor Grant <71059606+c6grant@users.noreply.github.com>
2026-01-14 18:07:14 -0700

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
+from datetime import datetime
 
 class TransactionBase(BaseModel):
     transaction_id: str
@@ -17,7 +18,7 @@ class TransactionCreate(TransactionBase):
 
 class TransactionResponse(TransactionBase):
     id: int
-    created_at: str
-    updated_at: Optional[str] = None
+    created_at: datetime
+    updated_at: Optional[datetime] = None
 
     model_config = ConfigDict(from_attributes=True)
